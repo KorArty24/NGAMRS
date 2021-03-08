8f24263dd35407f0756bc774026b9c8ddf2e6539
korArty2488 <48129496+KorArty24@users.noreply.github.com>
2021-03-08 18:24:03 +0300

    DataStructureIsReady MarkovChainToBeDone

--- src/NGrams.cpp
+++ src/NGrams.cpp
@@ -1,24 +1,84 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
-
-#include <cctype>
-#include <cmath>
-#include <fstream>
 #include <iostream>
-#include <string>
 #include "console.h"
-#include "hashmap.h"
+#include "ginteractors.h" // for GWindow
+#include "gwindow.h"
+#include <fstream>
+#include <iostream>// for GWindow
+#include "simpio.h"  // for getLine
+#include "vector.h"  // for Vector
 #include "queue.h"
-#include "vector.h"
+#include "stack.h"
+#include <map>
+#include <set>
+#include "foreach.h"
 
 using namespace std;
-
+const string ERROR_MSG="Can't open the file specified, please try again: ";
+const string PROMPT_FILE_MSG="Grid input file name? ";
+const string N_NGRAM_CHOICEDLG="Enter the number of words in NGRAM: ";
+string promptUserFile(ifstream &infile, string prompt);
+void createMap(map<Queue<string>, vector<string>> &map,ifstream &infile, int n_words);
+//void readFileContent(ifstream &infile, );
 int main() {
-    HashMap<Queue<string>,Vector<string>> map;
-    // TODO: Finish the program!
+
+    int numb=getInteger(N_NGRAM_CHOICEDLG);
+    //Map<Queue<string>, string> _map ;
+    map<Queue<string>, vector<string>> _map ;
 
 
-    cout << "Exiting." << endl;
+
     return 0;
 }
+
+string promptUserFile(ifstream &infile, string prompt){
+    while (true) {
+        cout << prompt;
+        string filename;
+        getline(cin, filename);
+        infile.open(filename.c_str());
+        if (!infile.fail()) return filename;
+        infile.clear();
+        cout<<ERROR_MSG <<endl;
+        if (prompt=="") prompt = PROMPT_FILE_MSG;
+
+    }}
+void createMap(map<Queue<string>, vector<string>> &map,ifstream &infile, int n_words){
+    string word;
+    string collocation=word;
+    vector<string> vec;
+    Queue<string> line;
+    Queue<string> que;
+    if (infile.is_open()){
+            while (infile.good()){
+                if (!infile.eof()){
+                    infile >> word;
+                    line.add(word);
+                    if (line.size()==n_words){
+                        que=line;
+                    }
+                    if (line.size()>n_words){
+                        line.dequeue();
+                        if(map.find(que)!=map.end()){
+                            vec.push_back(line.back());
+                            map.insert({que,vec});
+                        } else {map[que].push_back(line.back());}
+                    }
+    } else {
+                    infile.clear();
+                    infile.seekg (0, ios::beg);
+                while (line.size()!=n_words){
+                    infile >> word;
+                    line.add(word);
+
+                }
+                infile >>word;
+                vec.push_back(word);
+                map.insert({line,vec});
+                break;
+
+                }}
+            }}
+
+
+
+
