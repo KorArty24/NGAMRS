a5cd520ebb4c954cb81b59e93662047406ce40fb
korArty2488 <48129496+KorArty24@users.noreply.github.com>
2021-03-08 18:22:02 +0300

    InitialCommit

---
+++ src/NGrams.cpp
@@ -0,0 +1,24 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "console.h"
+#include "hashmap.h"
+#include "queue.h"
+#include "vector.h"
+
+using namespace std;
+
+int main() {
+    HashMap<Queue<string>,Vector<string>> map;
+    // TODO: Finish the program!
+
+
+    cout << "Exiting." << endl;
+    return 0;
+}
